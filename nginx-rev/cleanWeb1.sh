7f3cce263fab9eece0c321e18fe8ad1befeaf142
Giheok Nam <giheok.nam@gmail.com>
2017-03-17 11:49:33 -0700

    Add files via upload

---
+++ nginx-rev/cleanWeb1.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+function killitif {
+    docker ps -a  > /tmp/yy_xx$$
+    if grep --quiet $1 /tmp/yy_xx$$
+     then
+     echo "killing older version of $1"
+     docker rm -f `docker ps -a | grep $1  | sed -e 's: .*$::'`
+   fi
+}
+
+
+# Remove any existing containers, so we don't have failure
+# on the run command because of existing named containers. 
+
+killitif web1
+
